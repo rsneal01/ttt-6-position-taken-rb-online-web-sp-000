2aab2ed40ca3c783ae9626b89aa336edd6db7b6b
Bobby Neal <rsneal01@gmail.com>
2020-04-15 06:41:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,5 @@
 def position_taken?(board, index)
+<<<<<<< HEAD
   if board[index] == " " || board[index] == ""
     return false
   elsif board[index] == nil
@@ -6,5 +7,10 @@ def position_taken?(board, index)
   else board[index] == "X" || board[index] == "O"
     return true
   end
+=======
+  if board[0] != " "
+    return
+end
+>>>>>>> 4383023e2974d543a2f2d3fa913c8ed645ea8758
 end
 
